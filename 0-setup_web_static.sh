598cc020e9a267c39ea8e296a1fa265e7a21902a
Your Name <you@example.com>
2020-08-17 22:48:07 +0000

    0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,5 +16,7 @@ printf %s "
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "36 a\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-enabled/default
-chown -R ubuntu:ubuntu /data/
+sudo service nginx restart
+
