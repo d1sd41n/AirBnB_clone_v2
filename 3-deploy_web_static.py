541941ce0a307e1e1353ecfbe018122126626a5a
Daniel Alejandro <outnumbered@live.com>
2020-08-17 22:36:12 -0500

    3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+from fabric.api import *
+from datetime import datetime
+from os.path import isfile
+
+env.hosts = ['35.237.173.35', '34.75.253.25']
+
+
+def do_pack():
+    """ generates a tar file"""
+    try:
+        local("mkdir -p versions")
+        fecha = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        nombre = "versions/web_static_" + fecha + ".tgz"
+        local("tar -cvzf " + nombre + " web_static")
+        print("web_static packed: {} -> {}Bytes".format(nombre,
+              getsize(nombre)))
+        return nombre
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """deploys the
+    """
+    if isfile(archive_path):
+        pre_path = archive_path.split("/")[1]
+        put(archive_path, "/tmp/")
+        tmp_path = "/tmp/" + pre_path
+        releases_path = "/data/web_static/releases/" + pre_path.split(".")[0]
+        sudo("mkdir -p {:s}".format(releases_path))
+        sudo("tar -xzf {:s} -C {:s}".format(tmp_path, releases_path))
+        sudo("rm {:s}".format(tmp_path))
+        all_path_w = releases_path + "/web_static/*"
+        dictory_path = releases_path + "/web_static/"
+        sudo("mv {:s} {:s}".format(all_path_w, releases_path))
+        sudo("rm -rf {:s}".format(dictory_path))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {:s} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    else:
+        return False
+
+
+def deploy():
+	""" un comentario"""
+	d_pack = do_pack()
+	if d_pack is None:
+		return False
+	return do_deploy(d_pack)
